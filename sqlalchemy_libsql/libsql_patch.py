836ab9a5265c29b71131c388ba45f9e3a1a255b8
Niky V <nvoinovski@gmail.com>
2024-07-03 02:49:12 +0300

    Update libsql_patch.py

--- sqlalchemy_libsql/libsql_patch.py
+++ sqlalchemy_libsql/libsql_patch.py
@@ -1,1 +1,10 @@
+import sys
+from unittest.mock import patch
 
+def patch_libsql():
+    if sys.version_info >= (3, 12):
+        import sqlite3
+        with patch.object(sqlite3, 'SQLITE_BUSY_TIMEOUT', sqlite3.SQLITE_BUSY):
+            yield
+    else:
+        yield
